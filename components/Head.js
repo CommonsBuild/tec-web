258063becafdc1483f0322d4edff6cca36c6d542
Kristofer Lund <kristofer@fmckl.se>
2021-03-24 22:00:56 +0100

    Favicon

---
+++ components/Head.js
@@ -0,0 +1,46 @@
+import Head from "next/head";
+
+const HeadComponent = ({ title, description }) => {
+  return (
+    <Head>
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      <title>Sustainable & Ethical Design for Token Ecosystems</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://tecommons.org" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta
+        property="og:image"
+        content="https://tecommons.org/images/Share.jpg"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://tecommons.org" />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content="/images/Share.jpg" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
+  );
+};
+
+export default HeadComponent;
