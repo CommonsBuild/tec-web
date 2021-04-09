4fca133a6882dc74cdb6694d1c6645ef3c9fe08c
Kristofer Lund <kristofer@fmckl.se>
2021-04-09 13:18:34 +0200

    Add Freelio as partner

--- components/Collaborators.js
+++ components/Collaborators.js
@@ -5,6 +5,7 @@ const collaborators = [
   ["Block Science", "block.science"],
   ["cadCAD", "cadcad.org"],
   ["1Hive", "1hive.org"],
+  ["Freelio", "freel.io"],
 ];
 
 const Collaborator = ({ name, url }) => {
