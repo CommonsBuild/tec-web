6693f13a949f52ee54a1f8ee42f44a6af35a281c
Kristofer Lund <kristofer@fmckl.se>
2021-04-17 01:11:46 +0200

    css cleanup

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Footer from "../components/Footer";
 import Collaborators from "../components/Collaborators";
 import Head from "../components/Head";
 
-export default class Home extends Component {
+export default class Index extends Component {
   render() {
     return (
       <>
@@ -18,9 +18,9 @@ export default class Home extends Component {
           description="Advancing the field of token engineering in a regenerative way. Stewarding the cultivation of resources to accelerate the creation of robustly engineered models, modular tools, standards and educational opportunities."
         />
         <Navigation />
-        <div className="w-full text-white tec-bg-gradient-2 font-bai-jamjuree">
+        <div className="w-full leading-normal text-white tec-bg-gradient-2 font-bai-jamjuree">
           <div className="pb-20 bg-right-top bg-no-repeat bg-contain bg-tec-image-1 sm:pb-32">
-            <div className="container px-10 text-xl leading-normal lg:max-w-6xl lg:mx-auto sm:px-15 sm:text-3xl lg:text-4xl">
+            <div className="text-xl tec-container sm:text-3xl lg:text-4xl">
               <h1 className="pt-10 pb-6 font-semibold uppercase break-words stroke-1 tec-text-outline-blue-1 sm:tec-text-outline-blue-2 text-scale lg:text-8xl sm:pt-20 sm:pb-10">
                 <span className="whitespace-nowrap">Token</span>{" "}
                 <span className="whitespace-nowrap">Engineering</span>{" "}
@@ -35,34 +35,34 @@ export default class Home extends Component {
           </div>
 
           <div className="pb-20 bg-right-top bg-no-repeat bg-cover bg-tec-image-2 sm:pb-32">
-            <div className="container max-w-2xl px-10 sm:px-0 sm:flex sm:flex-row sm:mx-auto">
+            <div className="max-w-2xl tec-container sm:flex sm:flex-row sm:mx-auto">
               <div className="text-right sm:w-1/2 sm:pr-16 lg:pr-24">
                 <a name="about" />
-                <h2 className="pb-8 text-lg leading-normal sm:text-xl lg:text-2xl">
+                <h2 className="pb-8 text-lg sm:text-xl lg:text-2xl">
                   To·ken En·gi·neer·ing{" "}
                   <span className="whitespace-nowrap">/ˈtōkən/</span>{" "}
                   <span className="whitespace-nowrap">/enjəˈniriNG/</span>
                 </h2>
               </div>
-              <div className="text-sm leading-normal sm:w-1/2 sm:text-base">
+              <div className="text-sm sm:w-1/2 sm:text-base">
                 <Markdown>{attributes.tengineeringBody}</Markdown>
               </div>
             </div>
 
-            <div className="container max-w-2xl px-10 pb-20 sm:px-0 sm:flex sm:flex-row sm:mx-auto sm:pb-32">
+            <div className="max-w-2xl pb-20 tec-container sm:flex sm:flex-row sm:mx-auto sm:pb-32">
               <div className="text-right sm:w-1/2 sm:pr-16 lg:pr-24">
-                <h2 className="pb-8 text-lg leading-normal sm:text-xl lg:text-2xl">
+                <h2 className="pb-8 text-lg sm:text-xl lg:text-2xl">
                   Com·mons <span className="whitespace-nowrap">/ˈkämənz/</span>
                 </h2>
               </div>
-              <div className="text-sm leading-normal sm:w-1/2 sm:text-base">
+              <div className="text-sm sm:w-1/2 sm:text-base">
                 <Markdown>{attributes.commonsBody}</Markdown>
               </div>
             </div>
 
             <div className="bg-right-top bg-no-repeat bg-contain bg-tec-image-3">
               <div className="bg-right-top bg-no-repeat bg-auto bg-tec-image-4">
-                <div className="container w-full max-w-xl px-10 text-lg leading-normal sm:mx-auto sm:px-0 sm:text-xl">
+                <div className="max-w-xl text-lg tec-container sm:mx-auto sm:text-xl">
                   <a name="mission" />
                   <h1 className="pb-6 text-5xl font-semibold uppercase break-words tec-text-outline-unicorn-2 sm:tec-text-outline-unicorn-4 sm:text-8xl sm:pb-10">
                     {attributes.missionHeader}
@@ -79,7 +79,7 @@ export default class Home extends Component {
 
           <div className="pb-20 bg-right-top bg-no-repeat bg-contain bg-tec-image-5 sm:pb-32">
             <div className="pb-20 bg-right-top bg-no-repeat bg-auto bg-tec-image-6 sm:pb-32">
-              <div className="container w-full max-w-3xl px-10 text-2xl leading-normal text-center sm:mx-auto sm:text-xl">
+              <div className="max-w-3xl text-2xl text-center tec-container sm:mx-auto sm:text-xl">
                 <h1 className="pb-6 text-5xl font-semibold uppercase break-words tec-text-outline-unicorn-2 sm:tec-text-outline-unicorn-4 sm:text-8xl sm:pb-10">
                   {attributes.defiHeader}
                 </h1>
@@ -87,13 +87,13 @@ export default class Home extends Component {
               </div>
             </div>
 
-            <div className="container max-w-3xl px-10 sm:px-0 sm:flex sm:flex-row sm:mx-auto">
+            <div className="max-w-3xl tec-container sm:flex sm:flex-row sm:mx-auto">
               <div className="sm:text-right sm:w-1/2">
                 <h2 className="pb-6 text-3xl font-semibold break-words tec-text-outline-blue-1 sm:tec-text-outline-blue-2 sm:text-5xl sm:pb-10">
                   <Markdown>{attributes.radHeader}</Markdown>
                 </h2>
               </div>
-              <div className="text-base leading-normal sm:w-1/2 sm:text-lg sm:pl-8 lg:pl-16 sm:pr-10 lg:pr-0">
+              <div className="text-base sm:w-1/2 sm:text-lg sm:pl-8 lg:pl-16 sm:pr-10 lg:pr-0">
                 <Markdown>{attributes.commonsBody}</Markdown>
                 <YellowButton
                   text="Build With Us"
@@ -104,7 +104,7 @@ export default class Home extends Component {
           </div>
 
           <div className="pb-20 bg-right-top bg-no-repeat bg-contain bg-tec-image-7 sm:pb-32">
-            <div className="container w-full px-10 text-lg leading-normal sm:max-w-lg lg:max-w-4xl lg:mx-auto sm:pl-10 lg:pr-72 sm:text-xl">
+            <div className="max-w-xl text-lg tec-container sm:mx-auto sm:text-xl lg:pr-32">
               <a name="community" />
               <h1 className="pb-6 text-4xl font-semibold uppercase break-words tec-text-outline-unicorn-2 sm:tec-text-outline-unicorn-4 sm:text-7xl sm:pb-10">
                 {attributes.communityHeader}
@@ -118,7 +118,7 @@ export default class Home extends Component {
           </div>
 
           <div className="bg-right-top bg-no-repeat bg-cover bg-tec-image-8">
-            <div className="container w-full px-10 pb-20 text-lg leading-normal sm:max-w-lg lg:max-w-4xl lg:mx-auto sm:pl-10 lg:pr-72 sm:text-xl sm:pb-24">
+            <div className="pb-20 text-lg tec-container lg:pr-72 sm:text-xl sm:pb-24 lg:max-w-4xl">
               <a name="ecosystem" />
               <h1 className="pb-6 text-4xl font-semibold uppercase break-words tec-text-outline-blue-1 sm:tec-text-outline-blue-2 sm:text-7xl sm:pb-10">
                 Ecosystem
@@ -128,7 +128,7 @@ export default class Home extends Component {
 
             <Collaborators />
 
-            <div className="px-10 pb-20 sm:px-0 sm:pl-10 lg:mx-auto lg:max-w-4xl sm:pb-20">
+            <div className="pb-20 tec-container lg:max-w-4xl">
               <YellowButton
                 text="Join The Ecosystem"
                 url="https://forum.tecommons.org/"
